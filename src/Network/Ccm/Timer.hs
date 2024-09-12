330db755f0d43741064ecc58d27f9ea6dc7d1057
octalsrc <nicklewchenko92@gmail.com>
2024-09-12 16:45:49 -0600

    Add Timer module

---
+++ src/Network/Ccm/Timer.hs
@@ -0,0 +1,43 @@
+module Network.Ccm.Timer
+  ( Timer
+  , forkTimer
+  , awaitTimer
+  , tryTimer
+  , cancelTimer
+  ) where
+
+import Control.Monad.IO.Class
+import Control.Concurrent
+import Control.Concurrent.STM
+
+data Timer
+  = Timer
+    { timerVar :: TMVar ()
+    , timerThread :: ThreadId
+    }
+
+{- | Create a timer that is counting down the given number of microseconds -}
+forkTimer :: (MonadIO m) => Int -> m Timer
+forkTimer micros = do
+  v <- liftIO newEmptyTMVarIO
+  tid <- liftIO . forkIO $ do
+    threadDelay micros
+    atomically $ putTMVar v ()
+  return (Timer v tid)
+
+{- | An 'STM' action that blocks until the timer expires. -}
+awaitTimer :: Timer -> STM ()
+awaitTimer t = takeTMVar (timerVar t)
+
+{- | An 'STM' action that checks whether the timer has expired, without
+   blocking. -}
+tryTimer :: Timer -> STM Bool
+tryTimer t = do
+  r <- tryTakeTMVar (timerVar t)
+  case r of
+    Just () -> return True
+    Nothing -> return False
+
+{- | Kill the timer thread. -}
+cancelTimer :: (MonadIO m) => Timer -> m ()
+cancelTimer t = liftIO $ killThread (timerThread t)
